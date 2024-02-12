5cab507ce565c13e000b7bc4212e6abc3a464bd4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-02-12 20:49:05 +0530

    Rainbow code

--- color_changer.js
+++ color_changer.js
@@ -1,23 +1,25 @@
-//alert("FIND ME")
+alert("Click and find the MESSAGE")
 
 var bg = document.querySelector("body")
 var header = document.querySelector("h1")
+var paragraph = document.querySelector("p")
 var counter = 0
 
-const intr = setInterval("changeBackgroundColor()", 100);
+var intr = setInterval("changeBackgroundColor()", 150);
 
-header.addEventListener('mousedown', function() {
+header.addEventListener('mousedown', function () {
     header.style.color = "blue"
     header.textContent = "THE MAGIC IS IN SELF BELIEF";
-    bg.bgColor = "white";
+    bg.bgColor = "black";
+    header.style.color = "#1cfc03";
+    paragraph.style.color = "#1cfc03";
     clearInterval(intr);
 })
-
 
 function changeBackgroundColor() {
     rainbow = ["#ff0000", "#ff8317", "#fffb00", "#1cfc03", "#005eff", "#00d9ff", "#e100ff"]
     colorInput = rainbow[counter++]
-    if(counter === 7) {
+    if (counter === 7) {
         counter = 0
     }
     const date = new Date();
