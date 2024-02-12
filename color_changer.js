4b64df6b02b9718f03f8e0bf066ef732b5bf0430
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-02-12 20:57:49 +0530

    Rainbow code

---
+++ color_changer.js
@@ -0,0 +1,30 @@
+alert("Click and find the MESSAGE hidden inside the Rainbow!!!")
+
+var bg = document.querySelector("body")
+var header = document.querySelector("h1")
+var paragraph = document.querySelector("p")
+var counter = 0
+
+var intr = setInterval("changeBackgroundColor()", 150);
+
+header.addEventListener('mousedown', function () {
+    header.style.color = "blue"
+    header.textContent = "THE MAGIC IS IN SELF BELIEF";
+    bg.bgColor = "black";
+    header.style.color = "#1cfc03";
+    paragraph.style.color = "#1cfc03";
+    clearInterval(intr);
+})
+
+function changeBackgroundColor() {
+    rainbow = ["#ff0000", "#ff8317", "#fffb00", "#1cfc03", "#005eff", "#00d9ff", "#e100ff"]
+    colorInput = rainbow[counter++]
+    if (counter === 7) {
+        counter = 0
+    }
+    const date = new Date();
+    document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+    bg.bgColor = colorInput;
+    header.style.color = colorInput;
+}
+
